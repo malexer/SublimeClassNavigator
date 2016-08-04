77a5f471ffaf44b00d10fe2796f635aa24399f1d
Oleksii Markov <omarkov@cogniance.com>
2016-08-04 13:50:03 +0300

    First version with pythod source support

---
+++ ClassNavigator.py
@@ -0,0 +1,117 @@
+# -*- coding: utf-8 -*-
+
+from bisect import bisect_left
+import sys
+
+import sublime
+import sublime_plugin
+
+
+PY2 = sys.version_info.major == 2
+
+if PY2:
+    from UserList import UserList
+else:
+    from collections import UserList
+
+
+FILTERS = {
+    'source.python': lambda s: '(…)' not in s and '(' in s,
+    None: lambda s: not s.startswith(' '),
+}
+
+
+class LazyList(UserList):
+    """Lazily access item from list.
+
+    Apply transformation function on access to the item.
+    """
+
+    def __init__(self, source_list, function):
+        self.data = source_list
+        self.function = function
+
+    def __getitem__(self, key):
+        return self.function(self.data[key])
+
+
+class ClassNavigatorGoToClassCommand(sublime_plugin.TextCommand):
+
+    def run(self, edit):
+        filter_func = FILTERS.get(self.syntax_name, FILTERS[None])
+
+        items = [item for item in self.view.symbols() if filter_func(item[1])]
+
+        if items:
+            self.locations, names = zip(*items)
+
+            self.start_position = self.view.viewport_position()
+
+            index = self.get_index_of_closest_region(
+                self.current_line, self.locations)
+
+            self.view.window().show_quick_panel(
+                items=names,
+                selected_index=index,
+                on_select=self.jump_to,
+                on_highlight=self.scroll_to,
+            )
+
+    def get_index_of_closest_region(self, current_line, regions):
+        """Get index of the item in `regions` which is closest to current
+        location of cursor (or first selection block).
+        """
+
+        # convert regions to line numbers
+        regions_lines = LazyList(
+            regions,
+            function=lambda r: self.view.rowcol(r.begin())[0],
+        )
+
+        index = bisect_left(regions_lines, current_line)
+
+        if index == 0 or index == len(regions):
+            return index
+
+        a = regions_lines[index - 1]
+        b = regions_lines[index]
+
+        if abs(current_line - a) < abs(current_line - b):
+            return index - 1
+        else:
+            return index
+
+    @property
+    def current_line(self):
+        selection = self.view.sel()
+        if selection:
+            return self.view.rowcol(selection[0].begin())[0]
+
+        return 0
+
+    @property
+    def syntax_name(self):
+        selection = self.view.sel()
+        if selection:
+            syntax_scope = self.view.scope_name(selection[0].begin())
+            return syntax_scope.split(' ')[0]
+
+    def scroll_to(self, index):
+        """Scroll screen to selected item."""
+
+        self.view.show_at_center(self.locations[index])
+
+    def jump_to(self, index):
+        """Jump to selected item: scroll screen and move cursor."""
+
+        if index == -1:  # canceled
+            self.view.set_viewport_position(self.start_position)
+        else:
+            position = sublime.Region(
+                self.locations[index].begin(),
+                self.locations[index].begin(),
+            )
+            self.view.sel().clear()
+            self.view.sel().add(position)
+
+            self.scroll_to(index)
--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# SublimeClassNavigator
+# Class Navigator
+
+Class Navigator helps to quickly go to class defenition in a current file.
+
+It is using "Goto Symbol..." with some additional filtering.
+
+Currently specific class detection is done for Python source only. All other
+sources will be trying to detect first-level items.
+
+Feel free to add more accurate class detection for other source types - check
+the `FILTERS` dict in the `ClassNavigator.py`.
+
+
+# Usage
+
+| Linux or Windows | [Ctrl]+[Shift]+[C] |
+| MacOS            | [⌘]+[Shift]+[C]   |
